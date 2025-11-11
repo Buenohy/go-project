d0bd158b5b6b8b872fe7b093ec122d456bce81a1
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-11-11 18:15:20 -0300

    feat: using net/https with StatusCode to see website online and using for

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -8,22 +8,24 @@ import (
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
+	for {
+		exibeMenu()
 
-	comando := leComando()
+		comando := leComando()
 
-	switch comando {
-		case 1:
-			iniciarMonitoramento()
-		case 2:
-			fmt.Println("Exibindo Logs...")
-		case 0:
-			fmt.Println("Saindo do programa")
-			os.Exit(0)
-		default:
-			fmt.Println("Não conheço este comando")
-			os.Exit(-1)
-	}
+		switch comando {
+			case 1:
+				iniciarMonitoramento()
+			case 2:
+				fmt.Println("Exibindo Logs...")
+			case 0:
+				fmt.Println("Saindo do programa")
+				os.Exit(0)
+			default:
+				fmt.Println("Não conheço este comando")
+				os.Exit(-1)
+		}
+		}
 }
 
 func exibeIntroducao(){
@@ -51,5 +53,10 @@ func iniciarMonitoramento() {
 	fmt.Println("Monitorando ...")
 	site := "https://www.alura.com.br"
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
+	
+	if resp.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
+	}
 }
