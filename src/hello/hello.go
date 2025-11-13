3859eeb2365a13c2ab79c836c0d3c99f9704d16d
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-11-13 17:34:33 -0300

    feat: WriteString and strconv to format bool

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -15,7 +16,7 @@ const delay = 5
 
 func main() {
 	exibeIntroducao()
-	registraLog("site-falso", false)
+	// registraLog("site-falso", false)
 	// leSitesDoArquivo()
 	for {
 		exibeMenu()
@@ -124,9 +125,12 @@ func leSitesDoArquivo() []string {
 }
 
 func registraLog(site string, status bool) {
-	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE, 0666)
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(arquivo)
+
+	arquivo.WriteString(site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
 }
