30955164dfda7ca896dea3576b8998b9ebcd1a1a
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-11-13 17:58:19 -0300

    feat: show logs

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -28,6 +28,7 @@ func main() {
 				iniciarMonitoramento()
 			case 2:
 				fmt.Println("Exibindo Logs...")
+				imprimeLogs()
 			case 0:
 				fmt.Println("Saindo do programa")
 				os.Exit(0)
@@ -133,4 +134,12 @@ func registraLog(site string, status bool) {
 	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05")+ " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
+}
+func imprimeLogs(){
+	arquivo, err := os.ReadFile("log.txt")
+
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Println(string(arquivo))
 }
